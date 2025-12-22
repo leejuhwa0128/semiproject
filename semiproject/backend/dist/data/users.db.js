744ed06ca5824e8db3f3db9a6776e26bcf42a08c
leejuhwa0128 <144797466+leejuhwa0128@users.noreply.github.com>
2025-12-22 19:51:55 +0900

    Delete semiproject directory

--- semiproject/backend/dist/data/users.db.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.users = void 0;
-// 임시 DB (메모리 저장)
-exports.users = [];
--- semiproject/backend/src/routes/main/main.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from "express";
-
-const router = Router();
-
-router.get("/", (req: Request, res: Response) => {
-  res.json({ message: "메인페이지입니다" });
-});
-
-export default router;
